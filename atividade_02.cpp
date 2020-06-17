1abd8c0340a164eed894c60276b3467535a11bd3
matheus050 <59782697+matheus050@users.noreply.github.com>
2020-06-17 10:16:37 -0300

    Update atividade_02.cpp

--- atividade_02.cpp
+++ atividade_02.cpp
@@ -4,21 +4,21 @@
 
 
 int main(int argc, char *argv[]){
-  using namespace std;
-  string mensagem;
-  ofstream arquivo_saida("saida.txt", ios::app);
+    using namespace std;
+    string mensagem;
+    ofstream arquivo_saida("saida.txt", ios::app);
 
 	if (argc == 1){
-		cout << "Uso:" << argv[0] << " add <mensagem>" << endl;
+        cout << "Uso:" << argv[0] << " add <mensagem>" << endl;
 	}
 	if(argc == 2 && string(argv[1]) == "add"){
-    cout << "Digite uma mensagem:" << endl;
-    getline(cin, mensagem);
-    arquivo_saida << mensagem << endl;
+        cout << "Digite uma mensagem:" << endl;
+        getline(cin, mensagem);
+        arquivo_saida << mensagem << endl;
 	}
 	if(argc == 3 && string(argv[1]) == "add"){
-    mensagem = argv[2];
-		arquivo_saida << mensagem << endl;
+        mensagem = argv[2];
+	arquivo_saida << mensagem << endl;
 	}
 	return 0;
 }
